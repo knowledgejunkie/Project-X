c570b0fd99846b5faa4ef1c800ef7e2ca86ceab1
pstorch <pstorch>
2004-10-31 21:46:35 +0000

    unix build scipt added

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#build.sh
+rm -f ProjectX.jar
+perl -pe 's|\\|/|g' sources.lst > sources-linux.lst
+mkdir -p build
+javac -O -classpath lib/commons-net-1.1.0/commons-net-1.1.0.jar -d build @sources-linux.lst
+cp resources/*.* build
+jar cfvm projectx.jar MANIFEST.MF -C build .
+#start von projectx
+java -jar projectx.jar
