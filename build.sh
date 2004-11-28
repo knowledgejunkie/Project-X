90c86980644bb4f897cfef5d1752bb8d411dba41
pstorch <pstorch>
2004-11-28 22:56:34 +0000

    removed conversion of sources.lst to linux-sources.lst. not necessary anymore

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #build.sh
 rm -f ProjectX.jar
-perl -pe 's|\\|/|g' sources.lst > sources-linux.lst
 mkdir -p build
-javac -O -classpath lib/commons-net-1.2.2.jar -d build @sources-linux.lst
+javac -O -classpath lib/commons-net-1.2.2.jar -d build @sources.lst
 cp resources/*.* build
 jar cfvm projectx.jar MANIFEST.MF -C build .
 #start von projectx
