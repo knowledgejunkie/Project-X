f13f86cf8e8158723afb7e81cc5ec391afd68d09
dvbmatt <dvbmatt>
2005-05-16 19:01:58 +0000

    *** empty log message ***

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#build.sh
-rm -f ProjectX.jar
-mkdir -p build
-javac -encoding "ISO-8859-1" -O -g:none -classpath lib/commons-net-1.3.0.jar:lib/jakarta-oro-2.0.8.jar -d build @sources.lst
-cp resources/* build
-jar cfvm ProjectX.jar MANIFEST.MF -C build .
