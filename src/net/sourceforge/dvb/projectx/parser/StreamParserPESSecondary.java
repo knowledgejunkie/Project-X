177d0007fb04e9ea946150047c6e5ac783698b2b
dvbmatt <dvbmatt>
2008-01-31 13:55:01 +0000

    Patch_to_0.90.4.00.b22;url-reading;kill process (endless loop);beep-toolkit exception;vdr 1.5.x dvb-sub adaption

--- src/net/sourceforge/dvb/projectx/parser/StreamParserPESSecondary.java
+++ src/net/sourceforge/dvb/projectx/parser/StreamParserPESSecondary.java
@@ -153,8 +153,6 @@ public class StreamParserPESSecondary extends StreamParserBase {
 		int offset;
 		int returncode = 0;
 
-		int tmp_value1 = 0;
-
 		byte[] pes_packet = new byte[0x10006];
 		byte[] buffered_data;
 
