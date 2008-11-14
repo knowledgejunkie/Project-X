40ba36f6410a49c2a5079a42e4c0faeb226dad3a
dvbmatt <dvbmatt>
2008-11-14 17:22:56 +0000

    Patch_to_0.90.4.00.b26;ts-fix

--- src/net/sourceforge/dvb/projectx/parser/StreamParserTS.java
+++ src/net/sourceforge/dvb/projectx/parser/StreamParserTS.java
@@ -997,6 +997,9 @@ public class StreamParserTS extends StreamParserBase {
 										type = "(ECM)"; 
 										break;
 
+									case 0x40: 
+									case 0x41: 
+									case 0x42: 
 									case 0x43: 
 									case 0x44: 
 									case 0x45: 
